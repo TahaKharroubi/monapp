6c417f82956b460da8af946dcdefdac49c19fa6c
TahaKharroubi <kharroubi.taha@draexlmaier.de>
2018-01-31 09:49:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ function SendMessage(sender_psid, response) {
     },
     "message": getAiml(response)
   }
-   console.log("Log X :"+JSON.parse(request_body));
+   console.log("Log X :"+request_body);
   
   // Send the HTTP request to the Messenger Platform
   request({
